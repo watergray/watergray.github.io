bfe1663bbae043372723401ecb10aca97e6838f1
물 <79109035+watergray@users.noreply.github.com>
2025-01-24 15:30:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 // script.js
 const characters = [
-    {
-        name: "고양이",
-        video: "assets/character/gato.webm",
-        icon: "assets/gatoicon.webm"
-    },
-    {
-        name: "토끼",
-        video: "assets/character/conejo.webm",
-        icon: "assets/conejoicon.webm"
-    }
+    { video: "assets/character/gato.webm" },
+    { video: "assets/character/conejo.webm" }
 ];
 
 let currentIndex = 0;
 
 // Elementos
 const characterVideo = document.getElementById("character-video");
-const characterName = document.getElementById("character-name");
 const leftButton = document.getElementById("left-button");
 const rightButton = document.getElementById("right-button");
 const clickSound = document.getElementById("click-sound");
 
-// Función para actualizar el personaje
+// Función para actualizar el video
 function updateCharacter(index) {
     const character = characters[index];
     characterVideo.src = character.video;
-    characterName.textContent = character.name;
 }
 
-// Avanzar o retroceder en la lista
+// Cambiar personaje con los botones
 function changeCharacter(direction) {
     clickSound.play();
     if (direction === "left") {
